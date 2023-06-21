17ca2a9a46adaeac9a7cb504ea31690212d8411c
Laura Marques <laura.lima.marques2002@hotmail.com>
2023-06-21 07:37:45 -0300

    Trabalhando com as bibliotecas

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pandas as pd
+
+df = pd.read_excel("planilha.xlsx")
+print(df.head(n=3))
+
+# f = open("new_file.txt", "x")
+
+f = open("new_file.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
