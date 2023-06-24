afe064f3f3ad4a8135c3d5f7d35918d57cbf55ff
Laura Marques <laura.lima.marques2002@hotmail.com>
2023-06-24 18:21:06 -0300

    criando os nós e separando as votações

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import pandas as pd
+import grafoPonderado as gp
 
-df = pd.read_excel("planilha.xlsx")
-print(df.head(n=3))
+#criando o grafo ponderado
+g = gp.GrafoPonderado()
+
+#pegando o arquivo com os dados dos votos
+caminho = input("Digite o caminho do arquivo com .xlsx: ")
+df = pd.read_excel(caminho)
+
+#criando os nós
+dicDeputados = {}
+for i in range(len(df)):
+    dicDeputados[df['deputado_id'][i]] = df['deputado_nome'][i]
+g.adiciona_nos(dicDeputados.keys())
+
+#separando as votações
+votacoes = []
+for i in range(len(df)):
+    votacoes.append(df['idVotacao'][i])
+votacoes = list(set(votacoes))
+
+
+    
 
 # f = open("new_file.txt", "x")
 
-f = open("new_file.txt", "a")
-f.write("Now the file has more content!")
-f.close()
+# f = open("new_file.txt", "a")
+# f.write("Now the file has more content!")
+# f.close()
 
